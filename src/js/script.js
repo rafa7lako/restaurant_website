420e56ad8cc6f5f8079583d601676702eda8351b
rafa7lako <103770511+rafa7lako@users.noreply.github.com>
2024-01-16 19:04:53 +0100

    worked on the about section

--- src/js/script.js
+++ src/js/script.js
@@ -129,3 +129,25 @@ addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", () => {
 addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", autoSlide);
 
 window.addEventListener("load", autoSlide);
+
+/**
+ * PARALLAX EFFECT
+ */
+
+const parallaxItems = document.querySelectorAll('[data-parallax-item]');
+
+let x, y;
+
+window.addEventListener('mousemove', (event) => {
+    x = (event.clientX / window.innerWidth * 10) - 5;
+    y = (event.clientY / window.innerHeight * 10) - 5;
+
+    x = x - (x * 2);
+    y = y - (y * 2);
+
+    for(let i=0, len = parallaxItems.length; i < len; i++) {
+        x=x*Number(parallaxItems[i].dataset.parallaxSpeed);
+        y=y*Number(parallaxItems[i].dataset.parallaxSpeed);
+        parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+    };
+});
